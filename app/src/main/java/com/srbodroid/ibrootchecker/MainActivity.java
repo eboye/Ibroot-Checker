e1c4b7cad93bf961f9c028db9c23ec17629fb3db
Vasilj Milosevic <eboyee@gmail.com>
2019-10-13 04:28:43 +0200

    Dark mode AndroidX

--- app/src/main/java/com/srbodroid/ibrootchecker/MainActivity.java
+++ app/src/main/java/com/srbodroid/ibrootchecker/MainActivity.java
@@ -1,14 +1,10 @@
 package com.srbodroid.ibrootchecker;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.scottyab.rootbeer.RootBeer;
 
